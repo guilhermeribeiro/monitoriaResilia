30c0265e38e9e76f1efc9d92e1721e69e0e72bc9
Guilherme Ribeiro <guilherme.ufma@gmail.com>
2022-07-12 18:32:04 -0300

    Atualização com os arquivos da monitoria do dia 12/07

--- T20/modulo2/monitoria_semana3.py
+++ T20/modulo2/monitoria_semana3.py
@@ -44,8 +44,55 @@
 #     lines = f.readlines()
 #     print(lines)
 
-lista = ['Teste', 'T20']
-with open('readme.txt', 'w') as f:
-    for line in lista:
-        f.write(line)
-        f.write('\n')
+# lista = ['Teste', 'T20']
+# with open('readme.txt', 'w') as f:
+#     for line in lista:
+#         f.write(line)
+#         f.write('\n')
+
+
+# 12/07 ---------------------------------------------------------
+
+with open('D:\\Arquivo de Programas\\workspace\\monitoriaResilia\\T20\\modulo2\\curriculo_exemplo.txt', 'r') as f:
+     lista_curriculo = f.readlines()
+
+# print(lista_curriculo)
+
+# lista_curriculo = str(lista_curriculo)
+
+# print(lista_curriculo)
+
+# var = 'python'
+
+# print(var in lista_curriculo)
+
+concatena_string = ''
+
+for valor in lista_curriculo:
+    concatena_string = concatena_string + valor.replace('\n','') + ' '
+
+# lista_curriculo = str(lista_curriculo)
+
+
+var = ['python', 'powerbi', 'sql', 'soft']
+
+candidatos_match = 0
+count = 0
+
+for i in var:
+    if i in concatena_string:
+        count = count + 1
+
+print(count)
+
+if count > 0:
+    candidatos_match = candidatos_match + 1
+
+print(candidatos_match)
+
+dict1 = {}
+
+dict1["resumo_candidato_x"] = concatena_string
+
+print(dict1)
+
